b1147adcc5109c679bdfd2f0390c2cbda3b2236a
shema jolivet gislain <shemajolivet@gmail.com>
2022-09-13 10:58:06 +0200

    get and post method

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from watchlist_app.views import movie_list
+from watchlist_app.views import movie_list,Get_movie
 
 urlpatterns = [
-    path('movie/', movie_list, name='movie-list')
+    path('movie/', movie_list, name='movie-list'),
+    path('<int:movie_id>',Get_movie, name='got-movie'),
 ]
+
+
