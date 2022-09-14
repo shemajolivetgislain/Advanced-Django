fb23ccc57f7965540601c0cabfb3f13d36c44bf2
shema jolivet gislain <shemajolivet@gmail.com>
2022-09-14 09:55:03 +0200

    serialization

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-
+from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.TextField()
+    description = serializers.CharField()
     active = serializers.BooleanField()
+
+ 
+
+# class MoviesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Movie
+#         fields = ['id', 'name', 'description', 'active']
--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from watchlist_app.api.views import movie_list,Get_movie
 
 urlpatterns = [
-    path('movie/', movie_list, name='movie-list'),
+    path('list/', movie_list, name='movie-list'),
     path('<int:movie_id>',Get_movie, name='got-movie'),
 ]
