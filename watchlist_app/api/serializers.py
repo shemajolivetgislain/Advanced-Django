8f3fcbb55e102eed9c7d3628bff17271cd82c4a1
shema jolivet gislain <shemajolivet@gmail.com>
2022-09-16 17:47:34 +0200

    validations

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,16 @@
+from wsgiref.validate import validator
+from xml.dom import ValidationErr
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+# funtion for validating field
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short")
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -16,8 +23,27 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+# validating all object to be not the same
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and description should be different")
+        else:
+            return data
+#    validating single field 
+    # def validate_name(self, value):
+    #     if len(value) < 3:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     return value
+        
+
 
 # class MoviesSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Movie
 #         fields = ['id', 'name', 'description', 'active']
+
+# types of Validation
+
+# 1. field level validation
+# 2. object level validation
+# 3. function level validation by using validators
