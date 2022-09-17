159693433f8a4646573b91884b84485f280eba6a
shema jolivet gislain <shemajolivet@gmail.com>
2022-09-17 09:48:58 +0200

    updating urls and admin page

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from watchlist_app.api.views import MovieList, MovieDetail
+from watchlist_app.api.views import WatchDetail, WatchList, streamPlatform
 # from watchlist_app.api.views import movie_list,Get_movie
 
 urlpatterns = [
-    path('list/', MovieList.as_view(), name='movie-list'),
-    path('<int:movie_id>',MovieDetail.as_view(), name='got-movie'),
+    path('list/', WatchList.as_view(), name='movie-list'),
+    path('<int:movie_id>', WatchDetail.as_view(), name='got-movie'),
+
+    path('stream', streamPlatform.as_view(), name='stream-list'),
 ]
