d87abc69cfa23d03659f3a8c9c9ba91f596f2889
shema jolivet gislain <shemajolivet@gmail.com>
2022-09-28 22:46:24 +0200

    review model

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -23,10 +23,12 @@ class WatchList(models.Model):
 class Review(models.Model):
     rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
+    description = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.rating
+        return str(self.rating )+ " || " + self.watchlist.title
     
+
